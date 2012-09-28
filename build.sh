9109cc0b28ee1ffe1ba275505df67efceab9f65c
Wesley Luyten <luytenwesley@gmail.com>
2012-09-28 18:41:41 +0200

    update build file

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-java -jar compiler/compiler.jar \
+cd $(dirname $0)
+
+java -jar /usr/local/bin/compiler/compiler.jar \
 	--js=src/lightsout.js \
-	--js_output_file=preview/lightsout.js \
+	--js_output_file=lightsout.js \
 	--output_wrapper "" \
 	#--compilation_level ADVANCED_OPTIMIZATIONS \
