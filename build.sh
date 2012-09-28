16f00500e3a622b9ccd7119623c1438fc7158242
Wesley Luyten <luytenwesley@gmail.com>
2012-09-28 19:34:39 +0200

    remove source files for gh-pages

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $0)
-
-java -jar /usr/local/bin/compiler/compiler.jar \
-	--js=src/lightsout.js \
-	--js_output_file=lightsout.js \
-	--output_wrapper "" \
-	#--compilation_level ADVANCED_OPTIMIZATIONS \
