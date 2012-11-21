bd96f54b510f36f1a04d89012e9881c45667d1a4
Wesley Luyten <luytenwesley@gmail.com>
2012-11-21 17:22:20 +0100

    change compiler

--- build.sh
+++ build.sh
@@ -2,8 +2,5 @@
 
 cd $(dirname $0)
 
-java -jar /usr/local/bin/compiler/compiler.jar \
-	--js=src/lightsout.js \
-	--js_output_file=lightsout.js \
-	--output_wrapper "" \
-	#--compilation_level ADVANCED_OPTIMIZATIONS \
+uglifyjs2 src/lightsout.js \
+	-o lightsout.js -m -c
